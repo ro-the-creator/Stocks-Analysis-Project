f713f34846a12740d383e4e53b29cdb5a15043d8
Marcy Student <Marcy_Student@MARCY-MBAir24.local>
2025-08-07 14:42:19 -0400

    final pushes for Stock_Analyzer.py

--- Backend/analysis_scripts/Py-Files/app.py
+++ Backend/analysis_scripts/Py-Files/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 from Stock_Analyzer import StockAnalyzer
 
-stock = StockAnalyzer()
-stock.run_all()
+
